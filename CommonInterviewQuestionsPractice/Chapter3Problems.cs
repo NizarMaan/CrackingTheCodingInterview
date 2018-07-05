20a658c111ed066ba2c5dd131818ba0ec45333c4
Nizar <maannizar@hotmail.com>
2018-07-05 00:34:35 -0600

    queue problems started

--- CommonInterviewQuestionsPractice/Chapter3Problems.cs
+++ CommonInterviewQuestionsPractice/Chapter3Problems.cs
@@ -21,6 +21,58 @@ namespace CommonInterviewQuestionsPractice
         public void RunProblems()
         {
             Console.WriteLine("===========================Chapter 3=============================");
+
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(7);
+            stack.Push(5);
+            stack.Push(2);
+            stack.Push(6);
+            stack.Push(4);
+
+            PrintStack(stack);
+            stack = SortStack(stack);
+            PrintStack(stack);
+        }
+
+
+        private void PrintStack(Stack stack)
+        {
+            for(int i = stack.top; i > -1; i--)
+            {
+                Console.WriteLine(stack.stack[i]);
+            }
+            Console.WriteLine('\n');
+        }
+
+        private void PrintQueue(Queue queue)
+        {
+            for (int i = queue.front; i < queue.rear; i++)
+            {
+                Console.WriteLine(queue.queue[i]);
+            }
+
+            Console.WriteLine('\n');
+        }
+
+        private Stack SortStack(Stack inputStack)
+        {
+            Stack tempStack = new Stack();
+            int tempVar = 0;
+
+            while (!inputStack.IsEmpty())
+            {
+                tempVar = inputStack.Pop();
+
+                while (!tempStack.IsEmpty() && tempVar > tempStack.Peek())
+                {
+                    inputStack.Push(tempStack.Pop());
+                }
+
+                tempStack.Push(tempVar);
+            }
+
+            return tempStack;
         }
 
         class Stack
@@ -57,7 +109,7 @@ namespace CommonInterviewQuestionsPractice
 
             public int Peek()
             {
-                if (Empty())
+                if (IsEmpty())
                 {
                     throw new ArgumentException("Stack is empty.", "stack");
                 }
@@ -65,9 +117,69 @@ namespace CommonInterviewQuestionsPractice
                 return stack[top];
             }
 
-            public bool Empty()
+            public bool IsEmpty()
             {
                 return top == -1;
+            }
+        }
+
+        class Queue
+        {
+            public int front, rear, size, capacity;
+            public int[] queue;
+
+            public Queue(int capacity)
+            {
+                this.capacity = capacity;
+                size = 0;
+                front = size;
+                rear = front;
+                queue = new int[capacity];
+            }
+
+            public bool IsFull()
+            {
+                return size == capacity;
+            }
+
+            public bool IsEmpty()
+            {
+                return size == 0;
+            }
+
+            public void Enqueue(int value)
+            {
+                if (IsFull())
+                {
+                    return;
+                }
+
+                size++;
+                rear++;
+                queue[rear] = value;
+            }
+
+            public int Dequeue()
+            {
+                if (IsEmpty())
+                {
+                    return default(int);
+                }
+
+                int value = queue[front];
+                front++;
+                size--;
+                return value;
+            }
+
+            public int GetFront()
+            {
+                return queue[front];
+            }
+
+            public int GetRear()
+            {
+                return queue[rear];
             }
         }
     }
